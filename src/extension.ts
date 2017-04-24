a18cfcd24c7048ad1ae16da0b7cf7dffc0a1989e
Jared Wyles <jared.wyles@gmail.com>
2017-04-24 17:19:27 +1000

    Refactoring and added a list of compilers to collect from

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,13 @@
 import * as vscode from 'vscode';
 import { RequestPromiseOptions, RequestPromise } from 'request-promise-native';
 import rp = require('request-promise-native');
-
 import ASMOutputContentProvider from './ASMOutputContentProvider';
+
+
+interface CacheEntry<T> {
+    readonly entry: T;
+    readonly lastUpdated: Date;
+}
 
 interface GodboltConfig {
     readonly baseURL: string; //todo url type
@@ -42,6 +47,17 @@ interface ExplorerOutput {
     readonly asm: Array<ASM>
 }
 
+interface Compiler {
+    readonly id: string
+    readonly name: string,
+}
+
+interface QuickPickWithUnderlying<T> extends vscode.QuickPickItem {
+    readonly item: T;
+}
+
+type Compilers = Array<Compiler>;
+
 type filters = "binary" | "labels" | "intel" | "comments"| "directives"; 
 
 type Source = string;
@@ -49,19 +65,68 @@ type Source = string;
 //Just discovered function interfaces, this could be better as one of them
 //lets see how this plays out
 interface GodboltCompiler {
-    compile(source: Source): Promise<ExplorerOutput>;
+    compile(source: Source, compilerID: string): Promise<ExplorerOutput>;
 }
 
 
-function CompileSource<T extends GodboltCompiler>(compiler: T, source: Source): Promise<ExplorerOutput> { 
+function AddResultsToCache<T>(key: string, entry: T, context: vscode.ExtensionContext) {
+    const value = {
+        entry: entry,
+        lastUpdated: new Date()
+    } as CacheEntry<T>;
+    context.globalState.update(key, value);
+}
+
+function AddCompilersToCache(entry: Compilers,  context: vscode.ExtensionContext) {
+    AddResultsToCache("compilers", entry, context);
+}
+
+//put cache stuff into a file by it self
+
+function GetCompilersFromCache(context: vscode.ExtensionContext) {
+    let x = context.globalState.get("compilers") as CacheEntry<Compilers>;
+    if(x === undefined) {
+        return [];
+    }
+    const expiryDate = new Date(x.lastUpdated);
+    expiryDate.setDate(expiryDate.getDate() + 1);
+    if(expiryDate < new Date()) {
+        return [];
+    }
+    return x.entry;
+}
+
+function CompileSource<T extends GodboltCompiler>(compiler: T, source: Source, compilerID: string): Promise<ExplorerOutput> { 
     //for now we just want to pass back the results, future will do some manipulation to map it and make pretty :D 
-    return compiler.compile(source);
+    return compiler.compile(source, compilerID);
 }
 
-function GetCompiledResult(source: Source) : Promise<ExplorerOutput> {
-    let config = CreateConfig(vscode.workspace.getConfiguration('godbolt'));
+function GetCompiledResult(source: Source, config: GodboltConfig, compilerID: string) : Promise<ExplorerOutput> {
     let compiler = new HTTPCompiler(config);
-    return CompileSource(compiler, source);
+    return CompileSource(compiler, source, compilerID);
+}
+
+function GetCompilers(context: vscode.ExtensionContext, config: GodboltConfig) : Promise<Compilers> {
+    const compilers = GetCompilersFromCache(context);
+    if(compilers.length == 0) {
+        return rp.get(`${config.baseURL}/compilers`, {           
+            headers: {
+                'Accept': 'application/json'
+            }}).then((compilers) => {
+                compilers = JSON.parse(compilers);
+                return compilers.map((x) => {
+                    return {
+                        id: x.id,
+                        name: x.name
+                    } as Compiler;
+                }
+            )
+        }).then((x) => {
+            AddCompilersToCache(x, context);
+            return x;
+        });
+    };
+    return Promise.resolve(compilers);
 }
 
 class HTTPCompiler implements GodboltCompiler {
@@ -83,50 +148,63 @@ class HTTPCompiler implements GodboltCompiler {
              
         }
     }
-    getURL(c: GodboltConfig) : string {
-        return `${this.config.baseURL}/compiler/${this.config.compilerID}/compile`; //TODO: url builder instead of this
+    getURL(c: GodboltConfig, id: string) : string {
+        return `${this.config.baseURL}/compiler/${id}/compile`; //TODO: url builder instead of this
     }
-    compile(source: Source): Promise<ExplorerOutput> {
-        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-        return rp.post(this.getURL(this.config), this.getOptions(this.config, source)).then((data) => { return (<ExplorerOutput>data) });
+    compile(source: Source, compilerID: string): Promise<ExplorerOutput> {
+        return rp.post(this.getURL(this.config, compilerID), this.getOptions(this.config, source)).then((data) => { return (<ExplorerOutput>data) });
     }
 }
 
+function GetSelectedText(editor: vscode.TextEditor) : string {
+    const selection = editor.selection;
+    return editor.document.getText(selection);
+}
 
+function GetEntireEditorText(editor: vscode.TextEditor) : string {
+    return editor.document.getText();
+}
+
+//todo: less arguments
+function RunEditorCommand(compiler: Promise<Compilers>, config: GodboltConfig, previewUri: vscode.Uri, provider: ASMOutputContentProvider, GetText:(e: vscode.TextEditor) => string) {
+    return () => {
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        compiler.then((x) => x.map(y => { return {
+                item: y,
+                label: y.name,
+                description: ""
+            } as QuickPickWithUnderlying<Compiler>
+        }))
+        .then(x => vscode.window.showQuickPick(x))
+        .then(x => {
+            if(x == undefined) {
+                throw("no user input");
+            }
+            return GetCompiledResult(GetText(editor), config, x.item.id)
+        }).
+        then(x => { console.log(x); return x; })
+        .then(x => provider.setContent(x.asm.reduce((acc, val) => { return acc + "<br />" + val.text } ,"")))
+        .then(() =>  { return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, `compiler output for something`); });
+    }
+}
 
 export function activate(context: vscode.ExtensionContext) {
-    let previewUri = vscode.Uri.parse('asm-preview://authority/asm-preview');
-	let provider = new ASMOutputContentProvider();
-	let registration = vscode.workspace.registerTextDocumentContentProvider('asm-preview', provider);
-    let selectionDisposable = vscode.commands.registerCommand('extension.exploreCompilersSelection', () => {
-        let editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return; // No open text editor
-        }
-        let selection = editor.selection;
-        let text = editor.document.getText(selection);
-        GetCompiledResult(text).then((data) => {
-            provider.setContent(data.asm.reduce((acc, val) => { return acc + "<br />" + val.text } ,""));
-        }).then(() =>  { return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, 'ASM output'); });
-    });
-
-    let documentDisposable = vscode.commands.registerCommand('extension.exploreCompilersFile', () => {
-        let editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return; // No open text editor
-        }
-        let text = editor.document.getText();
-        GetCompiledResult(text).then((data) => {
-            provider.setContent(data.asm.reduce((acc, val) => { return acc + "<br />" + val.text } ,""));
-        }).then(() =>  { return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, 'ASM output'); });
-    });
-
-
-
+    let config = CreateConfig(vscode.workspace.getConfiguration('godbolt'));
+    const compiler = GetCompilers(context, config);
+    const previewUri = vscode.Uri.parse('asm-preview://authority/asm-preview');
+    const provider = new ASMOutputContentProvider();
+	const registration = vscode.workspace.registerTextDocumentContentProvider('asm-preview', provider);
+    const selectionDisposable = vscode.commands.registerCommand('extension.exploreCompilersSelection', RunEditorCommand(compiler, config, previewUri, provider, GetSelectedText));
+    const documentDisposable = vscode.commands.registerCommand('extension.exploreCompilersFile', RunEditorCommand(compiler, config, previewUri, provider, GetEntireEditorText));
     context.subscriptions.push(selectionDisposable);
     context.subscriptions.push(documentDisposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
+export function deactivate() {  
+
 }
